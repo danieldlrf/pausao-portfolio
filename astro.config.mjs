ae85e4eac30edcec9cbb95d4c49cb0e3f5880ca6
ugstudios-games <ugstudios.games@gmail.com>
2026-08-28 13:49:24 +0200

    final?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pausao.onrender.com',
-  integrations: [sitemap()]
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+    }),
+  ],
 });
